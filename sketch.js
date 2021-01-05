6d23ce225c05d9b6efe9b6c09ab4445077bb4d54
lakshya2510 <pdayal001@gmail.com>
2021-01-05 17:54:10 +0530

    We added SlingShot

--- sketch.js
+++ sketch.js
@@ -17,7 +17,7 @@ function setup(){
 
 
     ground = new Ground(600,height,1200,20);
-    platform = new Ground(150, 305, 300, 250);
+    platform = new Ground(150, 350, 300, 150);
 
     box1 = new Box(700,320,70,70);
     box2 = new Box(920,320,70,70);
@@ -33,19 +33,16 @@ function setup(){
     box5 = new Box(810,160,70,70);
     log4 = new Log(760,120,150, PI/7);
     log5 = new Log(870,120,150, -PI/7);
-    log6 = new Log(200, 200, 100, PI/2);
 
-    bird = new Bird(100,100);
+    bird = new Bird(100,200);
 
-    chain1 = new Chain(bird.body,log6.body)
+    slingShot = new SlingShot(bird.body,{x: 200, y:200})
 }
 
 function draw(){
     background(backgroundImg);
     Engine.update(engine);
-    console.log(box2.body.position.x);
-    console.log(box2.body.position.y);
-    console.log(box2.body.angle);
+    
     box1.display();
     box2.display();
     ground.display();
@@ -62,7 +59,18 @@ function draw(){
     log5.display();
 
     bird.display();
-    log6.display();
     platform.display();
-    chain1.display();
+    slingShot.display();
+}
+
+function mouseDragged(){
+
+    Matter.Body.setPosition(bird.body,{x:mouseX, y:mouseY})
+
+
+}
+
+function mouseReleased(){
+
+    slingShot.fly()
 }
